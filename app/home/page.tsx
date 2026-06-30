4b19953cb2b7ae7171361a07cb2c32ec6f672258
blaze <blazerrryt@outlook.com>
2026-06-29 23:33:22 -0700

    key

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -24,7 +24,7 @@ const HomePage = () => {
 
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full -mt-4 md:-mt-6">
+      <div className="max-w-7xl w-full -mt-8 md:-mt-12 lg:-mt-14">
         <Hero showThem={!user} />
         <HorizontalFeatures />
         <Grid />
--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function RootPage() {
   // Show landing page for unauthenticated users
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full -mt-4 md:-mt-6">
+      <div className="max-w-7xl w-full -mt-8 md:-mt-12 lg:-mt-14">
         <Hero showThem={true} />
         <HorizontalFeatures />
         <Grid />
